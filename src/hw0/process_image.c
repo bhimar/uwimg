fba2e6a46d22da76f9cf284b8a3066c314c3c9e9
bhimar <Rehaan@DESKTOP-7HDI3EJ.localdomain>
2021-01-15 18:49:31 -0800

    finished hw0

--- src/hw0/process_image.c
+++ src/hw0/process_image.c
@@ -94,10 +94,72 @@ float three_way_min(float a, float b, float c)
 
 void rgb_to_hsv(image im)
 {
-    // TODO Fill this in
+    for (int i = 0; i < im.w; i++) {
+        for (int j = 0; j < im.h; j++) {
+            float r = get_pixel(im, i, j, 0);
+            float g = get_pixel(im, i, j, 1); 
+            float b = get_pixel(im, i, j, 2);
+
+            float V = three_way_max(r,g,b);
+            float m = three_way_min(r,g,b);
+            float C = V - m;
+            float S;
+            if (V == 0) {
+                S = 0;
+            }else {
+                S = C / V;
+            }
+            float H_p = 0;
+            float H = 0;
+
+            if (C == 0) {
+                H = 0;
+            } else if (V == r) {
+                H_p = (g-b)/C;
+            } else if (V == g) {
+                H_p = (b-r)/C + 2;
+            } else if (V == b) {
+                H_p = (r-g)/C + 4;
+            }
+
+            if (C != 0) {
+                if (H_p < 0) {
+                    H = H_p/6 + 1;
+                } else {
+                    H = H_p/6;
+                }
+            }
+
+            set_pixel(im, i, j, 0, H);
+            set_pixel(im, i, j, 1, S);
+            set_pixel(im, i, j, 2, V);
+        }
+    }
+}
+
+
+
+float f(float n, float V, float S, float H) {
+    float k = fmod((n + H * 6),6);
+    return V - V * S * MAX(0, three_way_min(k, 4-k, 1));
 }
 
 void hsv_to_rgb(image im)
 {
-    // TODO Fill this in
+    for (int i = 0; i < im.w; i++) {
+        for (int j = 0; j < im.h; j++) {
+            float H = get_pixel(im, i, j, 0);
+            float S = get_pixel(im, i, j, 1); 
+            float V = get_pixel(im, i, j, 2);
+
+            float r = f(5, V, S, H);
+            float g = f(3, V, S, H);
+            float b = f(1, V, S, H);
+
+            set_pixel(im, i, j, 0, r);
+            set_pixel(im, i, j, 1, g);
+            set_pixel(im, i, j, 2, b);
+        }
+    }
 }
+
